61e89e0d937bd486c446294b0b5044108e98dbaa
SONALI SAHU <sonaliisahu04@gmail.com>
2026-01-14 20:18:44 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAKkQ0GR_hEeo257BnSFzW3TbePQNjjk3Y",
-    authDomain: "smart-issue-board-e1343.firebaseapp.com",
-    projectId: "smart-issue-board-e1343",
-    storageBucket: "smart-issue-board-e1343.firebasestorage.app",
-    messagingSenderId: "416836858593",
-    appId: "1:416836858593:web:f4ac72d63ffd865955fda0",
-    measurementId: "G-72L7B3QXES"
+  apiKey: "AIzaSyCmqSuZgay5KSf4Syd-Gkge2E4apyGFRzQ",
+  authDomain: "issue-board-740d3.firebaseapp.com",
+  projectId: "issue-board-740d3",
+  storageBucket: "issue-board-740d3.firebasestorage.app",
+  messagingSenderId: "253227475129",
+  appId: "1:253227475129:web:4e09fa711aba733fe0fb68",
+  measurementId: "G-MJKSY2Y7J4"
 };
